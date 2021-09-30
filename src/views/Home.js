9c362109b6f2bec0b0cb4dabcf11fcbbd95bdb60
Sheron Dave Sañol <sdssanol@gmail.com>
2021-09-30 17:52:13 +0800

    updated client

--- src/views/Home.js
+++ src/views/Home.js
@@ -13,7 +13,7 @@ export default function Home() {
     return (
         <>
             <LoginForm />
-            <p className='my-2'>Admin Credentials: admin@test.com admin123</p>
+            <p className='my-2 text-center'>Admin Credentials: admin@test.com admin123</p>
             <Header 
                 textStart='Your go-to marketplace for'
                 highlightedText='designer toys'
